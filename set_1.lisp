0e5629c630979b7db35aa451de57a30fe771f8c0
kdbanman <kirby.banman@gmail.com>
2016-01-14 12:09:26 -0700

    solve problem 1.2

--- set_1.lisp
+++ set_1.lisp
@@ -1,7 +1,7 @@
 ;QUESTION 1
 ;xmember checks if the first argument is a member the second argument
 (defun xmember (query-element query-list)
-    (if (atom-or-nil query-list)
+    (if (atom query-list)
         nil
         (if (equals-first-element query-element query-list)
             t
@@ -12,102 +12,135 @@
 
 ;true iff query-element equals the first element of query-list
 (defun equals-first-element (query-element query-list)
-    (if (atom-or-nil query-list)
+    (if (atom query-list)
         nil
         (equal query-element (car query-list))
     )
 )
 
-;true iff the argument is an atom or an empty list
-(defun atom-or-nil (query-object)
-    (cond ((atom query-object) t)
-          ((null query-object) t)
-          (t nil)
+;QUESTION 2
+(defun flatten (source-list)
+    (if (atom source-list)
+        (cons source-list nil)
+        (if (one-element source-list)
+            (append (flatten (car source-list)) ())
+            (append (flatten (car source-list)) (flatten (cdr source-list)))
+        )
+    )
+)
+
+#|
+(defun atom-not-nil (query-list)
+    (and (atom query-list) (not (null query-list)))
+)
+|#
+
+;true iff one element in list, error if passed atom
+(defun one-element (query-list)
+    (cond ((null query-list) nil)
+          ((null (cdr query-list)) T)
+          (T nil)
     )
 )
 
 ;; TESTS ;;
-
-(defun should-true (id predicate)
-    (if predicate
-        nil
-        (print (format t "Test ~D failed~%" id))
+(defun test-all (test-list)
+    (if (null test-list)
+       nil
+       (test (car test-list) (cdr test-list))
     )
 )
 
-(defun should-false (id predicate)
-    (should-true id (not predicate))
+(defun test (single-test remaining-tests)
+    (if (eval single-test)
+        nil
+        (print single-test)
+    )
+    (test-all remaining-tests)
+)
+
+(defun should-true (predicate)
+    (if predicate
+        T
+        (and (princ "Test Failed:") nil)
+    )
+)
+
+(defun should-false (predicate)
+    (should-true (not predicate))
 )
 
 ;Question 1 Tests;
-;
-(should-true 1
-    (atom-or-nil 'a)
-)
-(should-true 2
-    (atom-or-nil 2)
-)
-(should-true 3
-    (atom-or-nil nil)
-)
-(should-false 4
-    (atom-or-nil '(2))
-)
-(should-false 5
-    (atom-or-nil '(()))
-)
+(test-all '(
+    (should-false
+        (equals-first-element () ())
+    )
+    (should-true
+        (equals-first-element () '(()))
+    )
+    (should-true
+        (equals-first-element 'a '(a b))
+    )
+    (should-false
+        (equals-first-element 'a '(b a))
+    )
+    (should-false
+        (equals-first-element 'a '((a) b))
+    )
+    (should-false
+        (equals-first-element '(a 2) '((a) 2))
+    )
+    (should-true
+        (equals-first-element '(a 2) '((a 2) 2))
+    )
 
-(should-false 6
-    (equals-first-element () ())
-)
-(should-true 7
-    (equals-first-element () '(()))
-)
-(should-true 8
-    (equals-first-element 'a '(a b))
-)
-(should-false 9
-    (equals-first-element 'a '(b a))
-)
-(should-false 10
-    (equals-first-element 'a '((a) b))
-)
-(should-false 11 
-    (equals-first-element '(a 2) '((a) 2))
-)
-(should-true 12
-    (equals-first-element '(a 2) '((a 2) 2))
-)
-
-(should-true 13
-    (xmember '(a 2) '((a 2) 2))
-)
-(should-true 14
-    (xmember '(a 2) '(4 (a 2) 2))
-)
-(should-true 15
-    (xmember '1 '(1))
-)
-(should-false 16
-    (xmember '1 '( (1) 2 3))
-)
-(should-true 17
-    (xmember '(1) '((1) 2 3))
-)
-(should-false 18
-    (xmember nil nil)
-)
-(should-true 19
-    (xmember nil '(nil))
-)
-(should-false 20
-    (xmember nil '((nil)))
-)
-(should-true 21
-    (xmember '(nil) '(1 2 3 (nil)))
-)
-(should-false 22
-    (xmember '(nil) '(nil))
-)
+    (should-true
+        (xmember '(a 2) '((a 2) 2))
+    )
+    (should-true
+        (xmember '(a 2) '(4 (a 2) 2))
+    )
+    (should-true
+        (xmember '1 '(1))
+    )
+    (should-false
+        (xmember '1 '( (1) 2 3))
+    )
+    (should-true
+        (xmember '(1) '((1) 2 3))
+    )
+    (should-false
+        (xmember nil nil)
+    )
+    (should-true
+        (xmember nil '(nil))
+    )
+    (should-false
+        (xmember nil '((nil)))
+    )
+    (should-true
+        (xmember '(nil) '(1 2 3 (nil)))
+    )
+    (should-false
+        (xmember '(nil) '(nil))
+    )
+))
 
 ;Question 2 Tests;
+(test-all '(
+    (should-true
+        (equal (flatten '(a b)) '(a b))
+    )
+    (should-true
+        (equal (flatten '(a (3) (5 a (9)) 9)) '(a 3 5 a 9 9))
+    )
+    (should-true
+        (equal (flatten '(a (b c) d)) '(a b c d))
+    )
+    (should-true
+        (equal (flatten '((((a))))) '(a))
+    )
+    (should-true
+        (equal (flatten '(a (b c) (d ((e)) f))) '(a b c d e f))
+    )
+))
